ce0680c4fb6dc5082c8021b2240131ad578f3d74
Raul Costa <raulcoast@gmail.com>
2023-04-03 21:01:31 -0300

    Update implementation

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -39,5 +39,13 @@ public class Program1 {
 		sellerDao.insert(newSeller);
 		System.out.println("Insertd! New id =" + newSeller.getId());
 	
+		System.out.println();
+		System.out.println("=== TEST #5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		 
+		
 	}
 }
